9a36f27290390148e68f8209003e6e6ff77437f4
lynsei mariel lariba <lynseimariel@gmail.com>
2025-02-07 09:36:09 +0800

    .

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -28,12 +28,83 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Factorial";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(282, 116);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(212, 23);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(375, 149);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(58, 15);
+            this.lbl.TabIndex = 2;
+            this.lbl.Text = "Factorial: ";
+            this.lbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(375, 178);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(37, 15);
+            this.lblSum.TabIndex = 4;
+            this.lblSum.Text = "Sum: ";
+            this.lblSum.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sum";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private Button button1;
+        private TextBox txtInput;
+        private Label lbl;
+        private Label lblSum;
+        private Button button2;
+    }
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -6,5 +6,41 @@ namespace lab2
 		{
 			InitializeComponent();
 		}
-	}
+		public int RecursiveFactorial(int num)
+		{
+			if(num == 0)
+			{
+				return 1;
+			}
+			return num * RecursiveFactorial(num-1);
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			int n = int.Parse(txtInput.Text);
+			int r = RecursiveFactorial((int)n);
+			lbl.Text = $"Factorial: {r}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+		public int RecursiveSum(int[] numbers, int n)
+		{
+			if (n == 0) { return numbers[0]; }
+			return numbers[n] + RecursiveSum(numbers, n-1);
+		}
+        private void button2_Click(object sender, EventArgs e)
+        {
+			int[] numbers = txtInput.Text.Split(',').Select(int.Parse).ToArray();
+			int r = RecursiveSum(numbers, numbers.Length - 1);
+			lblSum.Text = $"Sum: {r}";
+        }
+    }
 }
