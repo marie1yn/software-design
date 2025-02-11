679b052d821a45a1fd9148c1e01d23c96091e124
lynse <lynse@KEVINXDURANT>
2025-02-11 16:47:39 +0800

    week4

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -20,20 +20,50 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            btnShowInfo = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(202, 23);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(513, 404);
+            listBox1.TabIndex = 3;
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(86, 72);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(94, 29);
+            btnShowInfo.TabIndex = 2;
+            btnShowInfo.Text = "Show Info";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(btnShowInfo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox listBox1;
+        private Button btnShowInfo;
+    }
 }
